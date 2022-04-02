3eb064838eeec5bf24d5716476e4e01344deb194
Paul Griego <paulgriego029@gmail.com>
2022-04-01 20:13:00 -0600

    added function that creates div elements based on sqft of x*x

--- etchasketch.js
+++ etchasketch.js
@@ -1,13 +1,19 @@
-
-  function createDiv (numberGrid) 
+function createDiv (numberGrid) 
   {
-  while(i<numberGrid) {
+  while(i<(numberGrid*numberGrid)) {
   i++;
   const newDiv = document.createElement("div");
-  newDiv.className="Grid"
+  newDiv.className="Grid";
+  newDiv.style.backgroundColor="red";
+  newDiv.style.border= "thin solid black";
+  newDiv.style.height="30px";
+  newDiv.style.width="30px";
   document.getElementById("container").appendChild(newDiv);
+
   }
 } 
 let numberGrid=0;
 let i=0; 
-createDiv(16);
+createDiv(4);
+
+
