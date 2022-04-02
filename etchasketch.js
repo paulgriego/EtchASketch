9083b10a31fb015b21dce082bccddc93c92c1816
Paul Griego <paulgriego029@gmail.com>
2022-04-01 23:41:48 -0600

    updated functions

--- etchasketch.js
+++ etchasketch.js
@@ -3,17 +3,12 @@ function createDiv (numberGrid)
   while(i<(numberGrid*numberGrid)) {
   i++;
   const newDiv = document.createElement("div");
-  newDiv.className="Grid";
-  newDiv.style.backgroundColor="red";
-  newDiv.style.border= "thin solid black";
-  newDiv.style.height="30px";
-  newDiv.style.width="30px";
+  newDiv.className="grid";
   document.getElementById("container").appendChild(newDiv);
-
   }
 } 
 let numberGrid=0;
 let i=0; 
-createDiv(4);
+createDiv(16);
 
 
