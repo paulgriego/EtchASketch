8e81c1ac005a970f35e2ad92c224b1d422973b93
Paul Griego <paulgriego029@gmail.com>
2022-04-09 21:42:09 -0600

    added rainbow fc/button and enable grid outline

--- etchasketch.js
+++ etchasketch.js
@@ -6,18 +6,57 @@ function generateGrid (square) {
   for (i=0; i<grid; i++) {
     let squareGrid =document.createElement("div");
     squareGrid.className = "squaregrid";
+    squareGrid.id = "idsquaregrid";
     squareGrid.style.width = ((550)/(square)) + "px";
     container.appendChild(squareGrid);
   }
+  colorPicker();
+}
+  
+
+
+ function colorPicker() {
   let inputs = document.getElementsByClassName("squaregrid");
   for(let i = 0; i < inputs.length; i++) {
+   {
     inputs[i].addEventListener("mouseover",function(){inputs[i].style.backgroundColor = pickColor()})
+   }
   }
+ }
+
+ function gridOutline() {
+  let inputs = document.getElementsByClassName("squaregrid");
+  for(let i = 0; i < inputs.length; i++) {
+   {
+    inputs[i].style.outline = "1px grey solid"
+   }
+  }
+ }
+
+
+function pickRainbow() {
+   pickcolor="rainbow"
+   document.getElementById("rainbow").style.background='gold';
+   document.getElementById("colorchanger").style.background='white';
+}
+function pickRGB() {
+  pickcolor="rgb"
+  document.getElementById("colorchanger").style.background='gold';
+  document.getElementById("rainbow").style.background='white';
 }
 
+
+
 function pickColor () {
-      return rgb = document.getElementById("colorchanger").value;
-}
+     if (pickcolor=="rgb") {
+     return document.getElementById("colorchanger").value;
+     }
+     if (pickcolor=="rainbow")
+     return random='#'+Math.floor(Math.random()*16777215).toString(16);
+  }
+  
+
+
 
 function removeContainer() {
   var child = document.getElementById('container');
@@ -43,5 +82,6 @@ function clearCurrentGrid() {
     inputs[i].style.backgroundColor = "white";
   }
 }
-
+let pickcolor="rgb";
 generateGrid(16);
+colorPicker();
